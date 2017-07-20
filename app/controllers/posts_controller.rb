72709eb01cc9ba0222b880dd8934c37a4a59cd2c
Marcos Orozco <marcos@marcosoroz.co>
2017-07-19 20:39:43 -0700

    Cleaned up some errors from the tutorial in the posts controller so posts save and display correctly

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,12 @@ class PostsController < ApplicationController
 
   # Create action saves the post into database
   def create
-    @post = Post.new
-    if @post.save(post_params)
-      flash[:notice] = "Successfully created post!"
-      redirect_to post_path(@post)
-    else
-      flash[:alert] = "Error creating new post!"
+    @post = Post.new(post_params)
+		if @post.save
+			flash[:notice] = "Successfully created post!"
+			redirect_to post_path(@post)
+		else
+			flash[:alert] = "Error creating new post!"
       render :new
     end
   end
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
   def update
     if @post.update_attributes(post_params)
       flash[:notice] = "Successfully updated post!"
-      redirect_to post_path(@posts)
+      redirect_to post_path(@post)
     else
       flash[:alert] = "Error updating post!"
       render :edit
