109cdd5b26bb27e3cfa0bc1e5ceb676a384eb42d
Marcos Orozco <marcos@marcosoroz.co>
2017-07-21 07:55:00 -0700

    Added avatars and avatar uploader for admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSFR attacks by raising an exception
   protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  #Configure permitted paramaters for devise
+  def configure_permitted_parameters
+    added_attrs = [:email, :password, :password_comfirmation, :remember_me, :avatar, :avatar, :avatar_cache]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
 end
